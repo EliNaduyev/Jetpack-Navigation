c93f2cd5d5fd339230180cdf82e54824e326e780
eli <eli.naduyev@tandemg.com>
2022-07-20 13:55:14 +0300

    added shared/state flow example

--- app/src/main/java/com/example/services/jetpack_navigation/splash/SplashFragment.kt
+++ app/src/main/java/com/example/services/jetpack_navigation/splash/SplashFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavOptions
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.example.services.jetpack_navigation.R
 import com.example.services.jetpack_navigation.databinding.FragmentSplashBinding
 import com.example.services.jetpack_navigation.log
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashFragment : Fragment() {
@@ -56,17 +61,76 @@ class SplashFragment : Fragment() {
     }
 
     /**
-     * if uiEvents was arrived in the background he will not lost and the observe callback will be
+     * if uiEventsLiveData was arrived in the background he will not lost and the observe callback will be
      * triggered right after the screen is turned on
      */
     fun initObservers() {
-        vm.uiEvents.observe(viewLifecycleOwner){
-            when(it){
-                UiEvents.Next -> {
-                    log("${this::class.java.name} - Redirecting user to home screen")
-                    findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
-                }
+//        observeUiEventLiveData()
+//        observeUiEventSharedFlow()
+//        observeUiEventSharedFlowAccordingLifeCycle()
+        observeUiEventStateFlow()
+    }
+
+    /**
+     * if the fragment after onStop the liveData observer will wait when the app is ready to
+     * navigate the user to the next screen and the actual navigation will work properly
+     */
+    private fun observeUiEventLiveData(){
+        vm.uiEventsLiveData.observe(viewLifecycleOwner){
+            log("observeUiEventLiveData - LiveData event arrived")
+            onEvent(it)
+        }
+    }
+
+    /**
+     * in case app launched and user press lockscreen immediately the emit from viewModel will
+     * happened after onStop and will collected here, but because the fragment is after onStop
+     * the actual navigation will not happened, in the previous liveData example this will not happened
+     */
+    private fun observeUiEventSharedFlow(){
+        lifecycleScope.launch {
+            vm.uiEventsSharedFlow.collectLatest {
+                log("observeUiEventSharedFlow - SharedFlow event arrived")
+                onEvent(it)
             }
+        }
+    }
+
+    /**
+     * Lifecycle.State.STARTED - will ignore events that emitted after onStop
+     * Lifecycle.State.CREATED - will act like observeUiEventSharedFlow() example
+     */
+    private fun observeUiEventSharedFlowAccordingLifeCycle(){
+        collectLatestLifecycleFlow(vm.uiEventsSharedFlow){
+            log("observeUiEventSharedFlowAccordingLifeCycle - SharedFlow event arrived")
+            onEvent(it)
+        }
+    }
+
+    private fun observeUiEventStateFlow(){
+        collectLatestLifecycleFlow(vm.uiEventsStateFlow){
+            log("observeUiEventStateFlow - StateFlow event arrived")
+            if(it != null)
+                onEvent(it)
+            else
+                log("observeUiEventStateFlow - state flow is NULL - default value")
+        }
+    }
+
+    private fun onEvent(uiEvent: UiEvents) {
+        when(uiEvent){
+            UiEvents.Next -> {
+                log("${this::class.java.name} - Redirecting user to home screen")
+                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            }
+        }
+    }
+}
+
+fun <T> Fragment.collectLatestLifecycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    viewLifecycleOwner.lifecycleScope.launch {
+        viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collect)
         }
     }
 }
--- app/src/main/java/com/example/services/jetpack_navigation/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/services/jetpack_navigation/splash/SplashViewModel.kt
@@ -2,17 +2,51 @@ package com.example.services.jetpack_navigation.splash
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.services.jetpack_navigation.delayOperation
 import com.example.services.jetpack_navigation.log
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class SplashViewModel: ViewModel() {
-    val uiEvents = MutableLiveData<UiEvents>()
+    val uiEventsLiveData = MutableLiveData<UiEvents>()
+
+    private val _uiEventsSharedFlow = MutableSharedFlow<UiEvents>()
+    val uiEventsSharedFlow = _uiEventsSharedFlow.asSharedFlow()
+
+    val uiEventsStateFlow = MutableStateFlow<UiEvents?>(null)
+
 
     init {
         log("${this::class.java.name} - init is called")
+//        emitEventUsingSharedFlow()
+        emitEventUsingStateFlow()
+    }
+
+    private fun emitEventUsingLiveData(){
         delayOperation(3000){
-            uiEvents.value = UiEvents.Next
-            log("${this::class.java.name} - Next event arrived")
+            log("SplashViewModel - LIVE_DATA event arrived")
+            uiEventsLiveData.value = UiEvents.Next
+        }
+    }
+
+    private fun emitEventUsingSharedFlow(){
+        delayOperation(3000) {
+            viewModelScope.launch {
+                log("SplashViewModel - SHARED_FLOW event arrived")
+                _uiEventsSharedFlow.emit(UiEvents.Next)
+            }
+        }
+    }
+
+    private fun emitEventUsingStateFlow(){
+        delayOperation(3000) {
+            viewModelScope.launch {
+                log("SplashViewModel - STATE_FLOW event arrived")
+                uiEventsStateFlow.value = UiEvents.Next
+            }
         }
     }
 
